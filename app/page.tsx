4412c40f38001a09f9a437c0f7cc78a526cf786f
Didier Peran Ganthier <didierganthierperan@gmail.com>
2025-01-17 23:21:28 -0500

    add image and style to Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 
 
+import Image from 'next/image';
+
 export default function Home() {
   return (
-   <div className=""></div>
+   <div className="w-full h-20 bg-white py-5 px-10">
+    <div className="w-12 h-12 border-2 rounded-full border-black">
+      <Image src="https://cdn-icons-png.flaticon.com/128/3018/3018447.png" alt="Add Icon" width={48} height={48} />
+    </div>
+   </div>
   );
 }
